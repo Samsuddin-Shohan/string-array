3218e20f127bf5d98bbfdb4cb0dc156a0eb6d841
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-01-15 02:58:25 +0600

    array method added

---
+++ array1.js
@@ -0,0 +1,11 @@
+let array1 = [ 2,3,2,1,4,5,2,3,6,7];
+
+let array2 = [222,3333];
+array1.sort().reverse();
+console.log(array1);
+
+console.log(Array.isArray(array1));
+
+console.log(array1.indexOf(4));
+console.log(array1.includes(55));
+console.log(array1.concat(array2));
